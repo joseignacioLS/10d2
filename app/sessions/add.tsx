0a4ed7de26ce98b5904953483f080c8468e7b661
Jose <ls.joseignacio@gmail.com>
2026-05-05 17:53:48 +0200

    remove unused page

--- app/sessions/add.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/src/components/Core/Button";
-import Tiptap from "@/src/components/Core/TipTap";
-
-export default function Home() {
-    return <main>
-        <Tiptap />
-        <Button>Guardar</Button>
-    </main>
-}
