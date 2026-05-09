138c7830f01c395a724b9a34bab90ad3b8d92301
Jose <ls.joseignacio@gmail.com>
2026-05-09 17:00:58 +0200

    fix getGroupDetail import

---
+++ src/components/Group.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import { getGroupDetail } from "../api/ttrpg";
+import { useFetchData } from "../hooks/useFetchData";
+import { CrumbsHeader } from "./Core/CrumbsHeader";
+
+type Props = { groupId: string };
+
+export const Group: React.FC<Props> = ({ groupId }) => {
+  const {
+    data: group,
+    loading,
+    error,
+  } = useFetchData(getGroupDetail, [groupId]);
+
+  if (loading) {
+    return "Cargando...";
+  }
+
+  if (error !== null) {
+    return "Ha habido un error cargando el contenido";
+  }
+
+  return (
+    <section>
+      <CrumbsHeader title={group.name || ""} />
+      <section>
+        <h3>Miembros</h3>
+        <ul>
+          {group.members.map(({ id, name }) => {
+            return <li key={id}>{name}</li>;
+          })}
+        </ul>
+      </section>
+      <section>
+        <h3>Campañas</h3>
+        <ul>
+          {group.campaigns.map(({ id, name }) => {
+            return (
+              <li key={id}>
+                <Link href={`/campaigns/${id}`}>{name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </section>
+  );
+};
