cb8fcab76842c001e1357874bc6e3ac2cfd77ae3
Jose <ls.joseignacio@gmail.com>
2026-06-18 17:24:48 +0200

    improve annotations

--- src/components/TTRPG/Annotation.tsx
+++ src/components/TTRPG/Annotation.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const Annotation: React.FC<Props> = ({ text, author = "Unknown" }) => {
   return (
-    <UserTextBubble color={"#5941b1"}>
+    <UserTextBubble color={"#5941b1"} width="full">
       <span dangerouslySetInnerHTML={{ __html: text }}></span>
       <span>
         <i> - {author}</i>
